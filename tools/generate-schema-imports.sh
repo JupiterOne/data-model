9f2ce3ac4b9096c28df345f26b94f176709a51d4
Isaac Williams <isaac.williams@lifeomic.com>
2020-04-21 14:56:59 -0400

    Make generate-schema-imports.sh portable & stable

--- tools/generate-schema-imports.sh
+++ tools/generate-schema-imports.sh
@@ -2,14 +2,21 @@
 
 MARKER='// Schema Imports'
 
-sed -i "" -e "\|$MARKER|,$ {" -e "\|$MARKER|b" -e d -e "}" src/IntegrationSchema.ts
+function sed_delete_after_marker() {
+    sed -i"" -e "\|$MARKER|,$ {" -e "\|$MARKER|b" -e "${1}d" -e "}" src/IntegrationSchema.ts
+}
 
-for SCHEMA_FILENAME in $(ls src/schemas | sort); do
-    SCHEMA=$(echo $SCHEMA_FILENAME | cut -d . -f 1)
-    sed -i "" "\|$MARKER|a\\
-import $SCHEMA from \"./schemas/$SCHEMA_FILENAME\";\\
+sed_delete_after_marker
+
+for SCHEMA in $(ls src/schemas | sed "s/\.json$//" | sort -f); do
+    sed -i"" "\|$MARKER|a\\
+import $SCHEMA from \"./schemas/$SCHEMA.json\";\\
 IntegrationSchema.addSchema($SCHEMA);\\
 " src/IntegrationSchema.ts
+
+    # Stabilize output between MacOS and Linux by removing empty lines in text
+    # after the marker
+    sed_delete_after_marker "/^$/"
 done
 
 git add src/IntegrationSchema.ts
