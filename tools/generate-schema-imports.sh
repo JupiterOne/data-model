de9354e8126ea0938d6e672602893361041db503
Adam Williams <adam.williams@lifeomic.com>
2020-04-21 13:47:27 -0400

    Fix sed to work on Mac. Beware.

--- tools/generate-schema-imports.sh
+++ tools/generate-schema-imports.sh
@@ -2,14 +2,14 @@
 
 MARKER='// Schema Imports'
 
-sed -i "\|$MARKER|,$ { \|$MARKER|b; d }" src/IntegrationSchema.ts
+sed -i "" -e "\|$MARKER|,$ {" -e "\|$MARKER|b" -e d -e "}" src/IntegrationSchema.ts
 
-for SCHEMA_FILENAME in $(ls src/schemas); do
+for SCHEMA_FILENAME in $(ls src/schemas | sort); do
     SCHEMA=$(echo $SCHEMA_FILENAME | cut -d . -f 1)
-    sed -i "\|$MARKER|a\
-import $SCHEMA from \"./schemas/$SCHEMA_FILENAME\";\n\
-IntegrationSchema.addSchema($SCHEMA);
-    " src/IntegrationSchema.ts
+    sed -i "" "\|$MARKER|a\\
+import $SCHEMA from \"./schemas/$SCHEMA_FILENAME\";\\
+IntegrationSchema.addSchema($SCHEMA);\\
+" src/IntegrationSchema.ts
 done
 
 git add src/IntegrationSchema.ts
