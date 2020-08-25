200c400fd7e7df3733a3a1aa089d5ed86ef3350d
Nick Dowmon <nick.dowmon@jupiterone.com>
2020-08-25 19:23:46 -0400

    add DENIES relationship class

--- src/RelationshipClass.ts
+++ src/RelationshipClass.ts
@@ -2,54 +2,66 @@
  * Standardized values for relationship `_class`.
  */
 export enum RelationshipClass {
-  ALLOWS = 'ALLOWS',
-  ASSIGNED = 'ASSIGNED',
-  CONNECTS = 'CONNECTS',
-  CONTAINS = 'CONTAINS',
+  /**
+   * A relationship between an AccessPolicy and a resource Entity.
+   */
+  ALLOWS = "ALLOWS",
+
+  /**
+   * A relationship between a {AccessRole, User, Group} and AccessPolicy.
+   */
+  ASSIGNED = "ASSIGNED",
+  CONNECTS = "CONNECTS",
+  CONTAINS = "CONTAINS",
 
   /**
    * A relationship between a User and any Entity.
    */
-  CREATED = 'CREATED',
+  CREATED = "CREATED",
 
   /**
    * A relationship between a CodeRepo and Function.
    */
-  DEFINES = 'DEFINES',
-  
-  DEPLOYED = 'DEPLOYED',
+  DEFINES = "DEFINES",
+
+  /**
+   * A relationship between an AccessPolicy and a resource Entity.
+   */
+  DENIES = "DENIES",
+
+  DEPLOYED = "DEPLOYED",
 
   /**
    * A relationship between a {Vulnerability, Finding} and Weakness.
    */
-  EXPLOITS = 'EXPLOITS',
+  EXPLOITS = "EXPLOITS",
 
-  EXTENDS = 'EXTENDS',
+  EXTENDS = "EXTENDS",
 
   /**
    * A relationship between any Entity and {Finding, Incident}.
    */
-  GENERATED = 'GENERATED',
+  GENERATED = "GENERATED",
 
-  HAS = 'HAS',
-  IDENTIFIED = 'IDENTIFIED',
-  IMPLEMENTS = 'IMPLEMENTS',
-  IS = 'IS',
-  MANAGES = 'MANAGES',
-  MITIGATES = 'MITIGATES',
-  MONITORS = 'MONITORS',
-  OPENED = 'OPENED',
-  OWNS = 'OWNS',
-  PERFORMED = 'PERFORMED',
-  PROTECTS = 'PROTECTS',
-  PROVIDES = 'PROVIDES',
-  TRIGGERS = 'TRIGGERS',
-  TRUSTS = 'TRUSTS',
+  HAS = "HAS",
+  IDENTIFIED = "IDENTIFIED",
+  IMPLEMENTS = "IMPLEMENTS",
+  IS = "IS",
+  MANAGES = "MANAGES",
+  MITIGATES = "MITIGATES",
+  MONITORS = "MONITORS",
+  OPENED = "OPENED",
+  OWNS = "OWNS",
+  PERFORMED = "PERFORMED",
+  PROTECTS = "PROTECTS",
+  PROVIDES = "PROVIDES",
+  TRIGGERS = "TRIGGERS",
+  TRUSTS = "TRUSTS",
 
   /**
    * A relationship between a User and any Entity.
    */
-  UPDATED = 'UPDATED',
+  UPDATED = "UPDATED",
 
-  USES = 'USES',
-};
+  USES = "USES",
+}
