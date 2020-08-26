5a6dcd062f3d7b72cd7cbf753673439e1a8b7b7e
Nick Dowmon <nick.dowmon@jupiterone.com>
2020-08-26 12:44:21 -0400

    Add linting rule "singleQuote": true

--- src/RelationshipClass.ts
+++ src/RelationshipClass.ts
@@ -5,7 +5,7 @@ export enum RelationshipClass {
   /**
    * A relationship between an {AccessPolicy, Firewall} and a resource Entity.
    */
-  ALLOWS = "ALLOWS",
+  ALLOWS = 'ALLOWS',
 
   /**
    * A relationship between a {AccessRole, User, Group} and AccessPolicy.
@@ -21,60 +21,60 @@ export enum RelationshipClass {
   /**
    * A relationship between a User and any Entity.
    */
-  CREATED = "CREATED",
+  CREATED = 'CREATED',
 
   /**
    * A relationship between a CodeRepo and Function.
    */
-  DEFINES = "DEFINES",
+  DEFINES = 'DEFINES',
 
   /**
    * A relationship between an {AccessPolicy, Firewall} and a resource Entity.
    */
-  DENIES = "DENIES",
+  DENIES = 'DENIES',
 
-  DEPLOYED = "DEPLOYED",
+  DEPLOYED = 'DEPLOYED',
 
   /**
    * A relationship between a {Vulnerability, Finding} and Weakness.
    */
-  EXPLOITS = "EXPLOITS",
+  EXPLOITS = 'EXPLOITS',
 
-  EXTENDS = "EXTENDS",
+  EXTENDS = 'EXTENDS',
 
   /**
    * A relationship between any Entity and {Finding, Incident}.
    */
-  GENERATED = "GENERATED",
+  GENERATED = 'GENERATED',
 
   /**
    * A relationship between a group or service Entity and a member of that Entity.
    * Closely related to or interchangeable with CONTAINS.
    */
-  HAS = "HAS",
+  HAS = 'HAS',
 
-  IDENTIFIED = "IDENTIFIED",
-  IMPLEMENTS = "IMPLEMENTS",
-  IS = "IS",
-  MANAGES = "MANAGES",
-  MITIGATES = "MITIGATES",
-  MONITORS = "MONITORS",
-  OPENED = "OPENED",
-  OWNS = "OWNS",
-  PERFORMED = "PERFORMED",
-  PROTECTS = "PROTECTS",
-  PROVIDES = "PROVIDES",
-  TRIGGERS = "TRIGGERS",
-  TRUSTS = "TRUSTS",
+  IDENTIFIED = 'IDENTIFIED',
+  IMPLEMENTS = 'IMPLEMENTS',
+  IS = 'IS',
+  MANAGES = 'MANAGES',
+  MITIGATES = 'MITIGATES',
+  MONITORS = 'MONITORS',
+  OPENED = 'OPENED',
+  OWNS = 'OWNS',
+  PERFORMED = 'PERFORMED',
+  PROTECTS = 'PROTECTS',
+  PROVIDES = 'PROVIDES',
+  TRIGGERS = 'TRIGGERS',
+  TRUSTS = 'TRUSTS',
 
   /**
    * A relationship between a User and any Entity.
    */
-  UPDATED = "UPDATED",
+  UPDATED = 'UPDATED',
 
   /**
    * A relationship between any Entity and a utility Entity.
    * The target entity can be "used" by many source entities.
    */
-  USES = "USES",
+  USES = 'USES',
 }
