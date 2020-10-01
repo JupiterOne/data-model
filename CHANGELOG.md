70938f755c5e27f27fbfb746eeb8e6ed2ce81ad8
Adam Williams <adam.williams@jupiterone.io>
2020-10-01 11:38:44 -0400

    Entity.id: string | number | (string | number)[]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,24 +10,33 @@ and this project adheres to
 
 ### Added
 
-- Added `createdBy`, `updatedBy`, `deletedBy`, `discoveredBy` to `Entity` schema
-- Added `RelationshipClass.SCANS` constant
+- Added `createdBy`, `updatedBy`, `deletedBy`, `discoveredBy` to `Entity`
+  schema.
+- Added `RelationshipClass.SCANS` constant.
+
+### Changed
+
+- `Entity.id` allows `string | number | (string | number)[]`. Though typically
+  it is best that Array properties are homogenious, `id` is a case where
+  different providers identify an Entity with different values, some using a
+  `string`, others a `number`. This change will allow `id` to capture the value
+  from each provider that tracks the Entity.
 
 ## 0.10.0 - 2020-09-09
 
 ### Added
 
-- Added `NS` as valid `DomainRecord`.`type` value
+- Added `NS` as valid `DomainRecord`.`type` value.
 
 ## 0.9.0 - 2020-08-26
 
 ### Added
 
-- Added `DENIES` relationship class
+- Added `DENIES` relationship class.
 
 ## 0.8.1 - 2020-08-20
 
 ### Changed
 
 - [#30](https://github.com/JupiterOne/data-model/issues/30) - Remove requirement
-  for `hostname` property in `Host` schema
+  for `hostname` property in `Host` schema.
--- src/validateEntityWithSchema.test.ts
+++ src/validateEntityWithSchema.test.ts
@@ -1,23 +1,78 @@
 import { validateEntityWithSchema } from './validateEntityWithSchema';
 
 test('throws error if schema does not exist', () => {
-  expect(
-    () => validateEntityWithSchema({ _class: ["ChimkenNumget"] })
-  ).toThrow("Could not find schema for class ChimkenNumget!");
+  expect(() => validateEntityWithSchema({ _class: ['ChimkenNumget'] })).toThrow(
+    'Could not find schema for class ChimkenNumget!',
+  );
 });
 
 test('throws error if entity fails to validate', () => {
-  expect(
-    () => validateEntityWithSchema({ _class: ["Account"] })
-  ).toThrow("Entity fails to validate as class 'Account'");
+  expect(() => validateEntityWithSchema({ _class: ['Account'] })).toThrow(
+    "Entity fails to validate as class 'Account'",
+  );
 });
 
 test('does not throw if entity successfully validates', () => {
-  expect(
-    () => validateEntityWithSchema({
-      _class: ["GraphObject"],
-      _key: "my_testing_key",
-      _type: "my_testing_type",
-    } as any)
+  expect(() =>
+    validateEntityWithSchema({
+      _class: ['GraphObject'],
+      _key: 'my_testing_key',
+      _type: 'my_testing_type',
+    } as any),
   ).not.toThrow();
+});
+
+describe('Entity', () => {
+  const requiredProperties = {
+    _class: ['Entity'],
+    _key: 'my_testing_key',
+    _type: 'my_testing_type',
+    name: 'John',
+    displayName: 'Wick',
+  };
+
+  test('allows string as id', () => {
+    expect(() =>
+      validateEntityWithSchema({
+        ...requiredProperties,
+        id: 'sumpinsumpin',
+      } as any),
+    ).not.toThrow();
+  });
+
+  test('allows string[] as id', () => {
+    expect(() =>
+      validateEntityWithSchema({
+        ...requiredProperties,
+        id: ['sumpinsumpin', 'nuttinnuttin'],
+      } as any),
+    ).not.toThrow();
+  });
+
+  test('allows number as id', () => {
+    expect(() =>
+      validateEntityWithSchema({
+        ...requiredProperties,
+        id: 123,
+      } as any),
+    ).not.toThrow();
+  });
+
+  test('allows number[] as id', () => {
+    expect(() =>
+      validateEntityWithSchema({
+        ...requiredProperties,
+        id: [123, 456],
+      } as any),
+    ).not.toThrow();
+  });
+
+  test('allows (string | number)[] as id', () => {
+    expect(() =>
+      validateEntityWithSchema({
+        ...requiredProperties,
+        id: ['gogogo', 456],
+      } as any),
+    ).not.toThrow();
+  });
 });
